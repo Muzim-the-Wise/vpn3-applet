e49a61ddf95b085e090cfd6c74f9def6444adf91
muzim <muzim@ubuntuDev.myguest.virtualbox.org>
2024-04-10 17:39:15 +0300

    Added functionality to read and display confing in ~/vpn3Confs

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
 const path = require('node:path')
 try {
 	require('electron-reloader')(module);
 } catch {}
-
+const util = require('node:util');
+const execProm = util.promisify(require('node:child_process').exec);
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -17,12 +18,31 @@ const createWindow = () => {
     win.loadFile('index.html')
   }
 
+  async function handleConfsLs() {
+    execProm('mkdir -p ~/vpn3Confs');
+    try{
+      const { stdout } = await execProm('ls ~/vpn3Confs/*.ovpn');
+      const confList = stdout.split("\n");
+      confList.pop();
+      return confList;
+    } catch (error) {
+      console.log(error.stderr);
+      return false;
+    }
+  }
+  // lsConfs().then(function(res) {
+  //   console.log(res);
+  // })
+  
+
+
   app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
   })
 
   app.whenReady().then(() => {
-    createWindow()
+    ipcMain.handle('getConfs', handleConfsLs);
+    createWindow();
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
       })
--- src/renderer.js
+++ src/renderer.js
@@ -1,5 +1,36 @@
-const confMenu = document.querySelector(".confMenu p");
+const confMenu = document.querySelector(".confMenu");
 
 confMenu.addEventListener("click", (event) =>{
-    console.log(event.target);
+    console.log(event.target.id);
 })
+
+const testDiv = document.querySelector("#testOutput");
+
+const confBut = document.querySelector("#confBut");
+const confDiv = document.querySelector("#confDiv")
+confBut.addEventListener("click", async (event) => {
+    while (confDiv.lastElementChild) {
+        confDiv.removeChild(confDiv.lastElementChild);
+    }
+    const confArr = await window.electronAPI.getConfs();
+    if (confArr == false) {
+        const node = document.createElement("p");
+        const textNode = document.createTextNode("No configs found in home/vpn3Confs");
+        node.appendChild(textNode);
+        confDiv.appendChild(node);
+        return 0;
+    }
+    for (const conf in confArr) {
+        const confNode = document.createElement("p");
+        const textNode = document.createTextNode(confArr[conf]);
+        confNode.appendChild(textNode);
+        confDiv.appendChild(confNode);
+        const connButNode = document.createElement("p");
+        const connTextNode = document.createTextNode('Connect');
+        connButNode.appendChild(connTextNode);
+        connButNode.classList.add("connBut");
+        confDiv.appendChild(connButNode);
+    }
+    return 1;
+})
+
